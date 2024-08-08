ca2ed4ca135157aaed76adcf88a97ea29770ad21
Ali Raza <aliraza.ar7785@gmail.com>
2024-08-08 22:09:28 +0500

    today

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const cors = require('cors')
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
 const app = express();
-
+const dbString = "mongodb+srv://devuser:devuser@cluster0.omanz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 app.use(cors());
+app.use(express.json());
 
-// API
-app.get('/api', (request, response) => {
-    response.send({ message: "Hello World!" })
+
+
+// Connect to MongoDB
+mongoose.connect(dbString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log('Error:', error);
 });
-app.get('/new', (request, response) => {
-    response.send({ message: "new hello World!" })
+
+// Routes
+app.post('/api/add-user', async (req, res) => {
+    try {
+        const { name, email, phone } = req.body;
+        const user = new User({ name: name, email: email, phone: phone });
+        await user.save();
+        res.json({ user: user, message: 'User added successfully', success: true });
+        // res.send({ user: user,message: 'User added successfully', success: true });
+    } catch (error) {
+        console.log('Error:', error);
+        res.json({ error, success: false });
+    }
 });
 
-
+app.get('/api/get-users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json({ users: users, success: true });
+    } catch (error) {
+        console.log('Error:', error);
+        res.json({ error, success: false });
+    }
+})
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
--- myapp/src/pages/Home.js
+++ myapp/src/pages/Home.js
@@ -1,30 +1,65 @@
-import React, { useState } from 'react'
-import Card from '../components/Card'
-import TopBar from '../components/common/TopBar'
-import { IoHome } from "react-icons/io5";
-import { MdOutlineAddHomeWork } from "react-icons/md";
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
-  const [data, setData] = useState()
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [data, setData] = useState([])
 
-  const handleFetchData = async () => {
+  const handleSubmit = async () => {
+    let data = {
+      name: name,
+      email: email,
+      phone: phone
+    }
     try {
-      const response = await axios.get("http://localhost:5000/new")
-      setData(response.data.message)
+      const response = await axios.post('http://localhost:5000/api/add-user', data)
+      console.log(response.data)
+      setName('')
+      setEmail('')
+      setPhone('')
+
     } catch (error) {
       console.log(error)
     }
   }
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/get-users')
+        setData(response.data.users)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }
+
+    , [])
+
   return (
     <div className=' container mx-auto'>
-
-
-      <div className='flex justify-center items-center cursor-pointer bg-black text-white' onClick={handleFetchData}>
-        Click Me
+      <div className='mt-10'>
+        <input value={name} onChange={(e) => setName(e.target.value)} className='p-2 mx-2 border-[1px] rounded-md' type='text' name='name' placeholder='Name' />
+        <input value={email} onChange={(e) => setEmail(e.target.value)} className='p-2 mx-2 border-[1px] rounded-md' type='text' name='email' placeholder='Email' />
+        <input value={phone} onChange={(e) => setPhone(e.target.value)} className='p-2 mx-2 border-[1px] rounded-md' type='text' name='phone' placeholder='Phone' />
       </div>
-      {data}
+      <button onClick={handleSubmit} className='p-2 mt-2 bg-blue-500 text-white rounded-md'>Submit</button>
+
+
+      {
+        data.map((user) => {
+          return (
+            <div key={user._id} className='mt-5'>
+              <h1>{user.name}</h1>
+              <p>{user.email}</p>
+              <p>{user.phone}</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
